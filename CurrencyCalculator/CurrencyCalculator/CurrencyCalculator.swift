0a5608d414b0bdb106f63dfe558c35ac7894ec7c
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-06-04 13:42:50 +0900

    [Refactor] Convert 함수 정의 및 Test 진행

--- CurrencyCalculator/CurrencyCalculator/CurrencyCalculator.swift
+++ CurrencyCalculator/CurrencyCalculator/CurrencyCalculator.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-final class CurrencyCalculator {
+enum CurrencyType: String {
+    case KRW
+    case USD
+}
 
+final class CurrencyCalculator {
+    func convertToUSD(amount: Int,
+                      from: CurrencyType,
+                      to: CurrencyType,
+                      rate: Int) -> Int {
+        
+        if from == .USD && to == .KRW {
+            return amount * rate
+        } else if from == .KRW && to == .USD {
+            return amount / rate
+        }
+        return amount
+    }
 }
--- CurrencyCalculator/CurrencyCalculatorTests/CurrencyCalculatorTests.swift
+++ CurrencyCalculator/CurrencyCalculatorTests/CurrencyCalculatorTests.swift
@@ -10,12 +10,60 @@ import XCTest
 
 final class CurrencyCalculatorTests: XCTestCase {
     
-    func testForTDDIHateYou() {
+    func testConvertKRWToUSD() {
         //Given
         let calculator = CurrencyCalculator()
         
+        //When
+        let from = CurrencyType.KRW
+        let to = CurrencyType.USD
+        let amount = 1300
+        let rate = 1300
+        
+        let result = calculator.convertToUSD(amount: amount,
+                                             from: from,
+                                             to: to,
+                                             rate: rate)
+        
         //Then
-        XCTAssertNotNil(calculator, "환율계산기 앱")
-//     XCTAssertNil(calculator, "환율계산기 앱")
+        XCTAssertEqual(result, 1, "1300원 환율 기준 1300원은 1달러 입니다.")
+    }
+    
+    func testConvertUSDToKRW() {
+        //Given
+        let calculator = CurrencyCalculator()
+        
+        //When
+        let from = CurrencyType.USD
+        let to = CurrencyType.KRW
+        let amount = 3
+        let rate = 1300
+        
+        let result = calculator.convertToUSD(amount: amount,
+                                             from: from,
+                                             to: to,
+                                             rate: rate)
+        
+        //Then
+        XCTAssertEqual(result, 3900, "3달러는 3900원.")
+    }
+    
+    func testConvertSameCurrency() {
+        //Given
+        let calculator = CurrencyCalculator()
+        
+        //When
+        let from = CurrencyType.KRW
+        let to = CurrencyType.KRW
+        let amount = 300000
+        let rate = 1300
+        
+        let result = calculator.convertToUSD(amount: amount,
+                                             from: from,
+                                             to: to,
+                                             rate: rate)
+        
+        //Then
+        XCTAssertEqual(result, amount, "같은 통화 기준")
     }
 }
