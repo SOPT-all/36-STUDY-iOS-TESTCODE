122f83eb0c20414e1edb8b72dec46d73814fd07d
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-06-04 18:28:45 +0900

    [green] 테스트 통과코드 작성

--- CurrencyCalculator/CurrencyCalculatorTests/CurrencyAPITests.swift
+++ CurrencyCalculator/CurrencyCalculatorTests/CurrencyAPITests.swift
@@ -6,13 +6,14 @@
 //
 
 import XCTest
+@testable import CurrencyCalculator
 
 final class CurrencyAPITests: XCTestCase {
 
     func testForTDDIHateTDD() {
-        let currency = ExchangeRateResponse()
+        let currency = ExchangeRateResponse(code: 200, message: "환율 조회 성공!", data: "1,450")
         
-        XCTAssertNil(currency)
+        XCTAssertNotNil(currency)
     }
     
 }
